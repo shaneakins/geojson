21b245f6fd7eacb0fc3e7ba9eb32791004591dba
Work_ub <Work_UB@Christophers-MacBook-Pro.local>
2022-05-02 15:12:53 +0200

    feat: added constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,8 @@
+export const MIN_MAX_RANGE = 0.0125
+
+export const VALID_RANGE = {
+  MIN_LAT: -90 + MIN_MAX_RANGE,
+  MIN_LNG: -180 + MIN_MAX_RANGE,
+  MAX_LAT: 90 - MIN_MAX_RANGE,
+  MAX_LNG: 180 - MIN_MAX_RANGE,
+}
