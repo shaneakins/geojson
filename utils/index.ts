3686214b141681d325cafe78039387d365eab782
Work_ub <Work_UB@Christophers-MacBook-Pro.local>
2022-05-02 15:15:05 +0200

    feat: converted max and min values for lat/lng to constants

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,7 @@
-export const isLatValid = (lat: number) => lat >= -90 && lat <= 90
-export const isLongValid = (long: number) => long >= -180 && long <= 180
+import { VALID_RANGE } from './constants'
+
+export const isLatValid = (lat: number) =>
+  lat >= VALID_RANGE.MIN_LAT && lat <= VALID_RANGE.MAX_LAT
+export const isLongValid = (long: number) =>
+  long >= VALID_RANGE.MIN_LNG && long <= VALID_RANGE.MAX_LNG
+export const randomId = Math.random().toString(36).slice(2)
