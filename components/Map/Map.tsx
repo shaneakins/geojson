5cf8cd8a1b6cfa4ef26ef04300c30e575d002c6e
Work_ub <Work_UB@Christophers-MacBook-Pro.local>
2022-05-02 10:23:50 +0200

    feat: added Map component

---
+++ components/Map/Map.tsx
@@ -0,0 +1,29 @@
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
+
+import { GeoJsonObject } from 'geojson'
+import { MapTypes } from '../../types/'
+
+const Map = ({ data, position }: MapTypes) => {
+  return (
+    <MapContainer
+      center={position}
+      zoom={5}
+      scrollWheelZoom={false}
+      style={{ height: '500px' }}
+      key={Math.random()}
+    >
+      <GeoJSON data={data} />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
+
+export default Map
