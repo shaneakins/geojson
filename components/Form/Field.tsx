e9823e67021b8342cd057caedd4f3d41010d7c12
Work_ub <Work_UB@Christophers-MacBook-Pro.local>
2022-05-02 10:18:42 +0200

    feat: added Field component

---
+++ components/Form/Field.tsx
@@ -0,0 +1,13 @@
+import { FieldTypes } from '../../types/'
+
+const Field = ({ label, name, register, required, validate }: FieldTypes) => (
+  <label>
+    <input
+      type='text'
+      placeholder={label}
+      {...register(name, { required: ' This is required', validate })}
+    />
+  </label>
+)
+
+export default Field
