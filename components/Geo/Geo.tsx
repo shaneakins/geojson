59dc700890ff1d076d4fd9ba326c23c9d6cbad72
Work_ub <Work_UB@Christophers-MacBook-Pro.local>
2022-05-03 09:55:50 +0200

    feat: refactored Geo to use useFetch; added loading and error messages

--- components/Geo/Geo.tsx
+++ components/Geo/Geo.tsx
@@ -1,50 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import osmtogeojson from 'osmtogeojson'
 import { GeoJsonObject } from 'geojson'
 import { LatLngTuple } from 'leaflet'
 import Form from '@/components/Form'
 import Map from '@/components/Map'
+import useFetch from 'hooks/useFetch'
+import { MIN_MAX_RANGE } from 'utils/constants'
 
 import { UserSubmitFormShort } from 'types/'
 
 export const Geo = () => {
   const [position, setPosition] = useState<LatLngTuple>([0, 0])
   const [geoData, setGeoData] = useState<GeoJsonObject>()
+  const [url, setUrl] = useState('')
+
+  const { data, loading, error } = useFetch<Document>(url, undefined, true)
 
   const submitData = (data: UserSubmitFormShort) => {
-    const MIN_MAX_RANGE = 0.0125
     const lat = +data.lat
     const lng = +data.lng
     const minLat = lat - MIN_MAX_RANGE
     const minLng = lng - MIN_MAX_RANGE
     const maxLat = lat + MIN_MAX_RANGE
     const maxLng = lng + MIN_MAX_RANGE
-    // const bbox = `${minLat},${minLng},${maxLat},${maxLng}`
     const bbox = `${minLng},${minLat},${maxLng},${maxLat}`
 
     setPosition([lat, lng])
-    fetchData(bbox)
+    setUrl(`http://www.openstreetmaps.org/api/0.6/map?bbox=${bbox}`)
   }
 
-  const fetchData = async (bbox: string) => {
-    try {
-      const res = await fetch(
-        `http://www.openstreetmaps.org/api/0.6/map?bbox=${bbox}`
-      )
-      const data = await res.text()
-      const xml = new window.DOMParser().parseFromString(data, 'text/xml')
-      const osmJson = osmtogeojson(xml)
+  useEffect(() => {
+    if (data) {
+      const osmJson = osmtogeojson(data)
       setGeoData(osmJson as GeoJsonObject)
-      console.log(osmJson)
-    } catch (err) {
-      console.log(err)
     }
-  }
+  }, [data])
 
   return (
     <>
       <Form submitData={submitData} />
-
+      {error && <p>There was an error loading the data</p>}
+      {loading && <p>Loading</p>}
       {geoData && <Map data={geoData} position={position} />}
     </>
   )
