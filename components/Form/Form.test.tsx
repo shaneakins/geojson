08f9cd24c1b5db7da2e0e0eafd9d137c39d9b109
Work_ub <Work_UB@Christophers-MacBook-Pro.local>
2022-05-04 09:00:59 +0200

    feat: refactored Form tests

--- components/Form/Form.test.tsx
+++ components/Form/Form.test.tsx
@@ -5,74 +5,93 @@ import Form from './Form'
 import userEvent from '@testing-library/user-event'
 import { VALID_RANGE } from '../../utils/constants'
 
-const submitData = jest.fn()
-const errorMsg = {
-  lat: `Latitude must be between ${VALID_RANGE.MIN_LAT} and ${VALID_RANGE.MAX_LAT}`,
-  lng: `Longitude must be between ${VALID_RANGE.MIN_LNG} and ${VALID_RANGE.MAX_LNG}`,
+const setup = () => {
+  const submitData = jest.fn()
+  const errorMsg = {
+    lat: `Latitude must be between ${VALID_RANGE.MIN_LAT} and ${VALID_RANGE.MAX_LAT}`,
+    lng: `Longitude must be between ${VALID_RANGE.MIN_LNG} and ${VALID_RANGE.MAX_LNG}`,
+  }
+  const utils = render(<Form submitData={submitData} />)
+  const latInput = utils.queryByPlaceholderText(/latitude/i) as Element
+  const lngInput = utils.queryByPlaceholderText(/longitude/i) as Element
+  const changeLatInput = (value: string) => userEvent.type(latInput, value)
+  const changeLngInput = (value: string) => userEvent.type(lngInput, value)
+  const buttonSubmit = utils.getByRole('button', { name: /plot/i })
+
+  return {
+    ...utils,
+    latInput,
+    lngInput,
+    changeLatInput,
+    changeLngInput,
+    buttonSubmit,
+    submitData,
+    errorMsg,
+  }
 }
 
-describe('RecipeForm', () => {
+describe('Form', () => {
   it('should render the basic fields', () => {
-    const { container } = render(<Form submitData={submitData} />)
-    expect(screen.queryByPlaceholderText(/latitude/i)).toBeInTheDocument()
-    expect(
-      screen.queryByPlaceholderText(/longitude/i)
-
-      // screen.getByRole('textbox', { name: /latitude/i })
-    ).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: /plot/i })).toBeInTheDocument()
+    const { container, latInput, lngInput, buttonSubmit } = setup()
+    expect(latInput).toBeInTheDocument()
+    expect(lngInput).toBeInTheDocument()
+    expect(buttonSubmit).toBeInTheDocument()
     expect(container).toMatchSnapshot()
   })
+
   it('should not show error when latitude is within allowed range', async () => {
-    render(<Form submitData={submitData} />)
-    const lat = screen.queryByPlaceholderText(/latitude/i) as Element
-    const submit = screen.getByRole('button', { name: /plot/i })
-    userEvent.type(lat, '0')
-    userEvent.click(submit)
+    const { changeLatInput, errorMsg, buttonSubmit } = setup()
+    changeLatInput('0')
+    userEvent.click(buttonSubmit)
+
     await waitFor(() => {
       expect(screen.queryByText(errorMsg.lat)).not.toBeInTheDocument()
     })
   })
+
   it('should show error when latitude is greater than allowed range', async () => {
-    render(<Form submitData={submitData} />)
-    const lat = screen.queryByPlaceholderText(/latitude/i) as Element
-    const submit = screen.getByRole('button', { name: /plot/i })
-    userEvent.type(lat, '999')
-    userEvent.click(submit)
+    const { changeLatInput, errorMsg, buttonSubmit } = setup()
+    changeLatInput('999')
+    userEvent.click(buttonSubmit)
     expect(await screen.findByText(errorMsg.lat)).toBeInTheDocument()
   })
+
   it('should show error when latitude is less than allowed range', async () => {
-    render(<Form submitData={submitData} />)
-    const lat = screen.queryByPlaceholderText(/latitude/i) as Element
-    const submit = screen.getByRole('button', { name: /plot/i })
-    userEvent.type(lat, '-999')
-    userEvent.click(submit)
+    const { changeLatInput, errorMsg, buttonSubmit } = setup()
+    changeLatInput('-999')
+    userEvent.click(buttonSubmit)
     expect(await screen.findByText(errorMsg.lat)).toBeInTheDocument()
   })
+
   it('should not show error when longitude is within allowed range', async () => {
-    render(<Form submitData={submitData} />)
-    const lng = screen.queryByPlaceholderText(/longitude/i) as Element
-    const submit = screen.getByRole('button', { name: /plot/i })
-    userEvent.type(lng, '0')
-    userEvent.click(submit)
+    const { changeLngInput, errorMsg, buttonSubmit } = setup()
+    changeLngInput('0')
+    userEvent.click(buttonSubmit)
+
     await waitFor(() => {
       expect(screen.queryByText(errorMsg.lng)).not.toBeInTheDocument()
     })
   })
+
   it('should show error when longitude is greater than allowed range', async () => {
-    render(<Form submitData={submitData} />)
-    const lng = screen.queryByPlaceholderText(/longitude/i) as Element
-    const submit = screen.getByRole('button', { name: /plot/i })
-    userEvent.type(lng, '999')
-    userEvent.click(submit)
+    const { changeLngInput, errorMsg, buttonSubmit } = setup()
+    changeLngInput('999')
+    userEvent.click(buttonSubmit)
     expect(await screen.findByText(errorMsg.lng)).toBeInTheDocument()
   })
+
   it('should show error when longitude is less than allowed range', async () => {
-    render(<Form submitData={submitData} />)
-    const lng = screen.queryByPlaceholderText(/longitude/i) as Element
-    const submit = screen.getByRole('button', { name: /plot/i })
-    userEvent.type(lng, '-999')
-    userEvent.click(submit)
+    const { changeLngInput, errorMsg, buttonSubmit } = setup()
+    changeLngInput('-999')
+    userEvent.click(buttonSubmit)
     expect(await screen.findByText(errorMsg.lng)).toBeInTheDocument()
+  })
+
+  it('should call submit function on submit', async () => {
+    const { changeLatInput, changeLngInput, buttonSubmit, submitData } = setup()
+    changeLatInput('40')
+    changeLngInput('-70')
+    userEvent.click(buttonSubmit)
+    await waitFor(() => expect(submitData).toHaveBeenCalledTimes(1))
   })
 })
